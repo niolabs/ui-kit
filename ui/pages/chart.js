3d038c4a649d31ecc1a3f7450418ae6674aaee3b
deliciousmonster <jaxon@deliciousmonster.com>
2018-03-16 11:46:05 -0600

    last one

--- ui/pages/chart.js
+++ ui/pages/chart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Code, Chart } from '../../dist';
+import { Code, Chart } from '@nio/ui-kit';
 
 const randomData = () => Array.from({ length: 10 }, () => Math.floor(Math.random() * 100));
 
