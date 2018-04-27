7b2c7021286ed38b3aa7261ecdbaf565e0b8b051
deliciousmonster <jaxon@deliciousmonster.com>
2018-04-27 09:00:37 -0600

    address deprecated @import css

--- src/components/ModalBody/index.js
+++ src/components/ModalBody/index.js
@@ -1,4 +1,4 @@
-import { Modal as ImportedComponent } from 'reactstrap';
+import { ModalBody as ImportedComponent } from 'reactstrap';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { themr } from 'react-css-themr';
